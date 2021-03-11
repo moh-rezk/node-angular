fc97dadb3d2158a2c7b344da3db7696b1101af70
moh-rezk <76524047+moh-rezk@users.noreply.github.com>
2021-03-11 05:59:25 +0200

    frist

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NotFoundComponent } from './component/not-found/not-found.component';
+import { ProfileComponent } from './component/profile/profile.component';
+import { SigninComponent } from './component/signin/signin.component';
+import { SignupComponent } from './component/signup/signup.component';
+import {AuthGuard} from './auth.guard'
+const routes: Routes = [
+  {path:'',redirectTo:'signin',pathMatch:'full'},
+  {path:'signin',component:SigninComponent},
+  {path:'signup',component:SignupComponent},
+  {path:'profile',component:ProfileComponent,canActivate:[AuthGuard]},
+  {path:'**',component:NotFoundComponent},
 
-const routes: Routes = [];
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
